c5c493189511e3554bf98b37bbfeb2b8302c0322
Valentin Heiderich <60042912+vh64g@users.noreply.github.com>
2023-05-15 22:16:43 +0200

    Update fotowiederstand.ino

--- fotowiederstand.ino
+++ fotowiederstand.ino
@@ -1,23 +1,30 @@
 //https://www.roboter-bausatz.de/projekte/lichtsensor-fotowiderstand-mit-arduino-auslesen
 
-int pinConfig[][10] = {{13},{A0}};
+int pinConfig[][10] = {{13},{A0, A1}};
 int pins = *(pinConfig + 1) - *pinConfig;
 
-float normal = 0;
+float normalGoalOne = 0;
+float normalGoalTwo = 0;
+
+int score[2] = {0,0};
 
 void setup() {
   Serial.begin(9600);
   for (int i=0; i<pins; i++){if(*((*(pinConfig))+i)==0){continue;};pinMode(*((*(pinConfig))+i), OUTPUT);}
   for (int i=0; i<pins; i++){if(*((*(pinConfig+1))+i)==0){continue;};pinMode(*((*(pinConfig+1))+i), INPUT);}
 
-  normal = analogRead(*((*(pinConfig+1))+1));
+  normalGoalOne = analogRead(*((*(pinConfig+1))+0));
+  normalGoalTwo = analogRead(*((*(pinConfig+1))+1));
 }
 
 void readSensor(){
-  float value = analogRead(*((*(pinConfig+1))+1));
-  if(value<(normal/2)){}
+  float GoalOne = analogRead(*((*(pinConfig+1))+0));
+  if(GoalOne<(normalGoalOne/2)){score[0] += 1;}      // Geht das mit  Pointern? Bzw geht das überhaupt weil es sich verändert?
+
+  float GoalTwo = analogRead(*((*(pinConfig+1))+1));
+  if(GoalTwo<(normalGoalTwo/2)){score[1] += 1;}
 }
 
 void loop() {
-  {readSensor();}
+  readSensor();
 }
